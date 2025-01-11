0df9355770d90a00fc9ea9de93d2129207d969ed
Sergiy <sergey.morenets@gmail.com>
2025-01-11 12:05:41 +0200

    Task 2

--- spring-task1/src/main/java/it/discovery/loader/SpringStarter.java
+++ spring-task1/src/main/java/it/discovery/loader/SpringStarter.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import it.discovery.repository.BookRepository;
+import it.discovery.service.BookService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import it.discovery.model.Book;
@@ -12,15 +13,15 @@ public class SpringStarter {
 	public static void main(String[] args) {
 		try (var context = new AnnotationConfigApplicationContext("it.discovery")) {
 			
-			BookRepository repository = context.getBean(BookRepository.class);
+			var service = context.getBean(BookService.class);
 			
 			Book book = new Book();
 			book.setName("Introduction into Spring 6");
 			book.setPages(100);
 			book.setYear(2023);
-			repository.saveBook(book);
+			service.saveBook(book);
 
-			List<Book> books = repository.findBooks();
+			List<Book> books = service.findBooks();
 			System.out.println(books);
 
 			System.out.println("Total bean count = " + context.getBeanDefinitionCount());
---
+++ spring-task1/src/main/java/it/discovery/service/BookServiceImpl.java
@@ -0,0 +1,52 @@
+package it.discovery.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import it.discovery.model.Book;
+import it.discovery.repository.BookRepository;
+import it.discovery.repository.DBBookRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@Service
+public class BookServiceImpl implements BookService {
+	private final BookRepository repository;
+	
+	private boolean cachingEnabled;
+	
+	private final Map<Integer, Book> bookCache = new ConcurrentHashMap<>();
+
+	public BookServiceImpl(BookRepository repository) {
+        this.repository = repository;
+        System.out.println("Using " + repository.getClass().getSimpleName() + " repository");
+	}
+	
+	@Override
+	public void saveBook(Book book) {
+		repository.saveBook(book);
+		
+		if(cachingEnabled) {
+			bookCache.put(book.getId(), book);
+		}
+	}
+	
+	@Override
+	public Book findBookById(int id) {
+		if(cachingEnabled && bookCache.containsKey(id)) {
+			return bookCache.get(id);
+		}
+
+		
+		return repository.findBookById(id);
+	}
+
+	@Override
+	public List<Book> findBooks() {
+		return repository.findBooks();
+	}
+}
