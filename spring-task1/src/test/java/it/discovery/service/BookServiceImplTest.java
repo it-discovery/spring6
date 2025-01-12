3e6aaccb15e1965bf5b4876fd8d00a80955153c7
Sergiy <sergey.morenets@gmail.com>
2025-01-12 15:26:35 +0200

    Task 12.2. Use Awaitility

--- spring-task1/src/test/java/it/discovery/service/BookServiceImplTest.java
+++ spring-task1/src/test/java/it/discovery/service/BookServiceImplTest.java
@@ -2,9 +2,13 @@ package it.discovery.service;
 
 import it.discovery.config.AppConfiguration;
 import it.discovery.model.Book;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -16,14 +20,17 @@ class BookServiceImplTest {
     BookService bookService;
 
     @Test
-    void findBookById_idCorrect_success() throws InterruptedException {
+    void findBookById_idCorrect_success() {
         Book book = new Book();
         book.setName("Introduction into Spring 6");
         book.setPages(100);
         book.setYear(2023);
         bookService.saveBook(book);
 
-        Thread.sleep(100);
+        Awaitility.await().atMost(100, TimeUnit.MILLISECONDS)
+                .pollInterval(Duration.ofMillis(10))
+                .pollInSameThread()
+                .until(() -> bookService.findBookById(book.getId()) != null);
 
         var book2 = bookService.findBookById(book.getId());
         assertNotNull(book2);
