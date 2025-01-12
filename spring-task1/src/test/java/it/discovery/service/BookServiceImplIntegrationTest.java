d7c115a51579ac4557965c283a1ccbd2880a38c9
Sergiy <sergey.morenets@gmail.com>
2025-01-12 15:51:18 +0200

    Task 13.1 Use Mockito functionality for mocks

--- spring-task1/src/test/java/it/discovery/service/BookServiceImplIntegrationTest.java
+++ spring-task1/src/test/java/it/discovery/service/BookServiceImplIntegrationTest.java
@@ -2,37 +2,38 @@ package it.discovery.service;
 
 import it.discovery.config.AppConfiguration;
 import it.discovery.model.Book;
-import it.discovery.repository.BookRepository;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig(AppConfiguration.class)
-class BookServiceImplTest {
-
-    @MockitoBean
-    BookRepository bookRepository;
+class BookServiceImplIntegrationTest {
 
     @Autowired
     BookService bookService;
 
     @Test
     void findBookById_idCorrect_success() {
-        int bookId = 2;
         Book book = new Book();
-        book.setId(bookId);
-        when(bookRepository.findBookById(bookId)).thenReturn(book);
+        book.setName("Introduction into Spring 6");
+        book.setPages(100);
+        book.setYear(2023);
+        bookService.saveBook(book);
+
+        Awaitility.await().atMost(100, TimeUnit.MILLISECONDS)
+                .pollInterval(Duration.ofMillis(10))
+                .pollInSameThread()
+                .until(() -> bookService.findBookById(book.getId()) != null);
 
         var book2 = bookService.findBookById(book.getId());
         assertNotNull(book2);
         assertSame(book, book2);
-
-        verify(bookRepository).findBookById(bookId);
     }
 }
